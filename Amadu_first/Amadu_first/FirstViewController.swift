9086548c2aeff5efedc38f04d91cd10c78187c0a
s-doris <swdoriz@gmail.com>
2020-03-16 22:32:35 +0900

    add to tableView

--- Amadu_first/Amadu_first/FirstViewController.swift
+++ Amadu_first/Amadu_first/FirstViewController.swift
@@ -8,12 +8,29 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UITableViewController {
+    
+    let celled = "celled"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
         
+        navigationItem.title = "친구"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: celled)
+    }
+    
+    override func tableView(_ tableView : UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView : UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: celled, for: indexPath)
+        cell.textLabel?.text = "SOMETHING"
+        
+        return cell
     }
     
 }
