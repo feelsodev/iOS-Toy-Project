a453c19420ade5e74a9a54e01c34c55ba4fb0fc7
s-doris <swdoriz@gmail.com>
2020-03-25 23:45:33 +0900

    add text fields to appointment view

--- Amadu_first/Amadu_first/Controllers/AddAppointmentListViewController.swift
+++ Amadu_first/Amadu_first/Controllers/AddAppointmentListViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class AddAppointmentListViewController: UIViewController {
 
+    let inputText : UITextField = {
+        let text = UITextField(frame: CGRect(x: 30, y: 300, width: 300, height: 30))
+        text.layer.borderWidth = 1.0
+        text.layer.borderColor = UIColor.black.cgColor
+        return text
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         self.navigationItem.title = "once"
-        
+        view.addSubview(inputText)
+//        inputText.backgroundColor = .red
     }
 
+    
 }
