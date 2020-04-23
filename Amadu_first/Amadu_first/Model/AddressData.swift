ef5d09f1c320268190fdcbdf907e0ba7fd52b5f0
s-doris <swdoriz@gmail.com>
2020-04-23 17:56:46 +0900

    Add variable bool type to AddressData

--- Amadu_first/Amadu_first/Model/AddressData.swift
+++ Amadu_first/Amadu_first/Model/AddressData.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 class AddressData{
+    var expand : Bool
     var title : String
     var time : String
     var personnel : String
     var address : String
     
-    init(title : String, time: String, personnel : String, address : String){
+    init(expand :Bool ,title : String, time: String, personnel : String, address : String){
+        self.expand = expand
         self.title = title
         self.time = time
         self.personnel = personnel
@@ -22,6 +24,6 @@ class AddressData{
     }
     
     static var dummyList : [AddressData] = [
-        AddressData(title: "홍대 나의봄날에서 만나기", time: "PM 02:30",personnel: "8명", address: "잔다리로 332")
+        AddressData(expand: false, title: "홍대 나의봄날에서 만나기", time: "PM 02:30",personnel: "8명", address: "잔다리로 332")
     ]
 }
