a9162e7ca5bb9ae411fc70e2f53ab1b07d8562c6
s-doris <swdoriz@gmail.com>
2020-05-12 23:31:38 +0900

    Add apple login

--- AppleLoginTest/AppleLoginTest/ViewController.swift
+++ AppleLoginTest/AppleLoginTest/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import AuthenticationServices
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, ASAuthorizationControllerPresentationContextProviding, ASAuthorizationControllerDelegate {
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupProviderLoginView()
     }
-
+    
+    
+    func setupProviderLoginView() {
+        let loginButton = ASAuthorizationAppleIDButton()
+        loginButton.frame = CGRect(x: 150, y: 250, width: 200, height: 40)
+        loginButton.addTarget(self, action: #selector(handleAuthorizationAppleIDButtonPress), for: .touchUpInside)
+        self.view.addSubview(loginButton)
+        
+    }
+    
+    @objc func handleAuthorizationAppleIDButtonPress(){
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.delegate = self
+        controller.presentationContextProvider = self
+        controller.performRequests()
+    }
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
 
 }
 
