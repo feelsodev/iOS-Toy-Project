b77c9d5042a4eafa9c7c8ede377ed3312108a49c
s-doris <swdoriz@gmail.com>
2020-05-23 20:06:14 +0900

    Fix UI set code

--- iOS-KakaoAddress/Pods/Target Support Files/Pods-iOS-KakaoAddress/Pods-iOS-KakaoAddress-frameworks.sh
+++ iOS-KakaoAddress/Pods/Target Support Files/Pods-iOS-KakaoAddress/Pods-iOS-KakaoAddress-frameworks.sh
@@ -199,12 +199,16 @@ fi
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${PODS_ROOT}/NMapsMap/framework/NMapsMap.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Then/Then.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${PODS_ROOT}/NMapsMap/framework/NMapsMap.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Then/Then.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
