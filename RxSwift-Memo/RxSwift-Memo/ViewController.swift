8b7d2288e3e5b91d5a6b2ca3cfca88122990710d
s-doris <swdoriz@gmail.com>
2020-05-24 23:48:41 +0900

    Add rxswift operator just test

--- RxSwift-Memo/RxSwift-Memo/ViewController.swift
+++ RxSwift-Memo/RxSwift-Memo/ViewController.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
-
+    
+    var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func exJust1(){
+        Observable.just("hello")
+            .subscribe(onNext: { str in
+                print(str)
+            })
+            .disposed(by: disposeBag)
+    }
 }
+
 
