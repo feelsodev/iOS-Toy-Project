5842ca9e8eec54879a85e175efb3e8e8dcab50c8
s-doris <swdoriz@gmail.com>
2020-05-31 01:31:12 +0900

    Init rxmemo-project

---
+++ RxSwift-Memo/RxSwift-Memo/ViewController/MemoListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MemoListViewController.swift
+//  RxSwift-Memo
+//
+//  Created by once on 2020/05/31.
+//  Copyright © 2020 once. All rights reserved.
+//
+
+import UIKit
+
+class MemoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ RxSwift-Memo/RxSwift-Memo/ViewModel/MemoListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MemoListViewModel.swift
+//  RxSwift-Memo
+//
+//  Created by once on 2020/05/31.
+//  Copyright © 2020 once. All rights reserved.
+//
+
+import Foundation
