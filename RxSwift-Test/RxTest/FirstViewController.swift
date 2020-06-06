28a1757e20c68e734eb26099ee64a8b6b6fbef31
s-doris <swdoriz@gmail.com>
2020-06-07 03:39:04 +0900

    RxSwift closure, delegate를 observable 처리

---
+++ RxSwift-Test/RxTest/FirstViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FirstViewController.swift
+//  test
+//
+//  Created by once on 2020/06/06.
+//  Copyright © 2020 once. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class FirstViewController: UIViewController {
+  let disposeBag = DisposeBag()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let btn = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+    view.addSubview(btn)
+    btn.backgroundColor = UIColor.brown
+
+    btn.rx.tap
+      .flatMap(selectedColor)
+      .observeOn(MainScheduler.instance)
+        .bind(to: view.rx.backgroundColor)
+//      .subscribe(onNext: { [weak self] (color) in
+//        self?.view.backgroundColor = color
+//      })
+    .disposed(by: disposeBag)
+  }
+
+  func selectedColor() -> Observable<UIColor> {
+    let vc = SecondViewController()
+    present(vc, animated: true)
+    return vc.selectedColor
+  }
+}
+
---
+++ RxSwift-Test/RxTest/SecondViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SecondViewController.swift
+//  test
+//
+//  Created by once on 2020/06/06.
+//  Copyright © 2020 once. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class SecondViewController: UIViewController {
+  let disposeBag = DisposeBag()
+  
+  private let selectedColorSubject = PublishSubject<UIColor>()
+  var selectedColor: Observable<UIColor> {
+    return selectedColorSubject.asObservable()
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = UIColor.gray
+  
+    let redButton = UIButton(frame: CGRect(x: 0, y: 100, width: 100, height: 100))
+    redButton.backgroundColor = UIColor.red
+    let blueButton = UIButton(frame: CGRect(x: 200, y: 100, width: 100, height: 100))
+    blueButton.backgroundColor = UIColor.blue
+  
+    view.addSubview(redButton)
+    view.addSubview(blueButton)
+  
+    let redObservable = redButton.rx.tap.map { UIColor.red }
+    let blueObservable = blueButton.rx.tap.map { UIColor.blue }
+  
+    Observable.of(redObservable, blueObservable)
+      .merge()
+    .bind(onNext: complete)
+//      .subscribe(onNext: { [weak self] in
+//       self?.complete(color: $0)
+//      })
+    .disposed(by: disposeBag)
+  }
+
+  func complete(color: UIColor) {
+    dismiss(animated: true) { [weak self] in
+      self?.selectedColorSubject.onNext(color)
+      self?.selectedColorSubject.onCompleted()
+    }
+  }
+}
