8823d8b813c8995e3bf8048dbe21e62e293f9788
s-doris <swdoriz@gmail.com>
2020-07-07 01:55:14 +0900

    Add page next button set : success

--- PageViewTest/PageViewTest/ViewController.swift
+++ PageViewTest/PageViewTest/ViewController.swift
@@ -24,12 +24,11 @@ class ViewController: UIViewController {
     func attribute() {
         view.backgroundColor = .white
         firstView.backgroundColor = .darkGray
-        
-//        completeBtn.setTitle("완료", for: .normal)
-//        completeBtn.setTitleColor(.black, for: .normal)
-//        completeBtn.layer.borderWidth = 1.0
-//        completeBtn.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-//        completeBtn.addTarget(self, action: #selector(addManna), for: .touchUpInside)
+        completeBtn.setTitle("완료", for: .normal)
+        completeBtn.setTitleColor(.black, for: .normal)
+        completeBtn.layer.borderWidth = 1.0
+        completeBtn.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        completeBtn.addTarget(self, action: #selector(btn), for: .touchUpInside)
     }
     
     func layout() {
@@ -38,28 +37,31 @@ class ViewController: UIViewController {
         pageView.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 70).isActive = true
         pageView.view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         pageView.view.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-//        pageView.view.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.7).isActive = true
         pageView.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
 
+        view.addSubview(completeBtn)
+        completeBtn.translatesAutoresizingMaskIntoConstraints = false
+        completeBtn.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5).isActive = true
+        completeBtn.bottomAnchor.constraint(equalTo: pageView.view.topAnchor, constant: 5).isActive = true
+        completeBtn.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
     }
     
-//    func clickBtn() {
-//        let vcs = pageView.VCArr
-//        
-//    }
+    func clickBtn() {
+        let currentView = pageView.VCArr
+        let currentPage = pageView.pageControl.currentPage
+        let nextPage = currentPage + 1
+        
+        if nextPage < currentView.count {
+            let nextVC = currentView[nextPage]
+            self.pageView.setViewControllers([nextVC], direction: .forward, animated: true) { _ in
+                self.pageView.pageControl.currentPage = nextPage
+            }
+            print("current page : \(nextPage)")
+        }
+    }
     
-//    func next() {
-//        let vcs = self.viewControllerList
-//        //or self.viewControllers
-//        let currentPage = self.pageControl.currentPage
-//        let nextPage = currentPage + 1
-//
-//        if nextPage < vcs.count {
-//            let nextVC = vcs[nextPage]
-//            self.setViewControllers([nextVC], direction: .forward, animated: true) { _ in
-//                self.pageControl.currentPage = nextPage
-//            }
-//        }
-//    }
+    @objc func btn(_ sender: Any) {
+        clickBtn()
+    }
 }
 
